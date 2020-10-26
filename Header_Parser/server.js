4209b8dda6146655c518175e2c1c6e25faba84d9
themagicbean <legumelegal@gmail.com>
2020-10-26 06:43:15 -0700

    Add files via upload

---
+++ Header_Parser/server.js
@@ -0,0 +1,48 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+/*I can get the IP address, preferred languages (from header Accept-Language) and system infos (from header User-Agent) for my device.*/
+
+//test: https://boilerplate-project-headerparser.themagicbean.repl.co/api/whoami
+
+app.get('/api/whoami', (req, res) => {
+  console.log(req.headers['accept-language']);
+
+  res.json({
+    "ipaddress": req.headers['x-forwarded-for'],
+    "language": req.headers['accept-language'],
+    "software": req.headers['user-agent']
+  })
+});
+
+//req.header(<header>)
